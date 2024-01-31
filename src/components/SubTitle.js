cce0e9af5573413861ae50a9da7cf88bd559ff95
maejyomi <141015562+maejyomi@users.noreply.github.com>
2024-01-31 17:33:04 +0900

    AboutMe 진행중
    - 폰트 적용
    - 서브 타이틀 컴포넌트 생성
    - 배경 이미지 적용

---
+++ src/components/SubTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SubTitle = ({ title }) => {
+  return (
+    <div className="text-5xl font-bold font-archivo mt-5 tracking-tighter underline decoration-orange-300">
+      {title}
+    </div>
+  );
+};
+
+export default SubTitle;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily : {
+      'archivo' : ["Archivo Black", "sans-serif"]
+    }
   },
   plugins: [require('@tailwindcss/forms')],
 }
