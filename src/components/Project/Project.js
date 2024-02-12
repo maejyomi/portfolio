24b0776d8bcacd3a6a1943a7317f12bc0717ea3a
maejyomi <141015562+maejyomi@users.noreply.github.com>
2024-02-12 23:21:09 +0900

    Project 업데이트
    - 프로젝트 아이템 컴포넌트 생성

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,15 +1,16 @@
 import React from "react";
 import SubTitle from "../SubTitle";
+import ProjectItem from "./ProjectItem";
 
 const Project = ({ elementRef }) => {
   return (
     <div ref={elementRef} className="bg-[#f1f2f6] py-[4rem]">
       <div className="flex flex-col justify-center items-center gap-5">
         <SubTitle title="PROJECT" />
-        <div className="bg-[#755567] w-full p-[2rem] py-[4rem] flex flex-wrap justify-center gap-5">
-          <div className="bg-white rounded-lg w-[350px] h-[400px] ">test</div>
-          <div className="bg-white rounded-lg w-[350px] h-[400px] ">test</div>
-          <div className="bg-white rounded-lg w-[350px] h-[400px] ">test</div>
+        <div className="bg-[#755567] w-full p-[2rem] py-[4rem] mt-[1rem] flex flex-wrap justify-center gap-5">
+          <ProjectItem />
+          <ProjectItem />
+          <ProjectItem />
         </div>
       </div>
     </div>
---
+++ src/components/Project/ProjectItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectItem = () => {
+  return <div className="bg-white rounded-lg w-[350px] h-[400px] ">test</div>;
+};
+
+export default ProjectItem;
