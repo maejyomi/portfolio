e2209998653a6398dbf48d6101b30c3f3760404c
maejyomi <141015562+maejyomi@users.noreply.github.com>
2024-02-19 10:08:51 +0900

    Project 업데이트
    - 프로젝트 아이템 내용 추가
    - 프로젝트 섹션 완

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,15 +2,31 @@ import React from "react";
 import SubTitle from "../SubTitle";
 import ProjectItem from "./ProjectItem";
 
+import Project1 from "../../images/프로젝트1.png";
+import Project2 from "../../images/프로젝트2.png";
+import Project3 from "../../images/프로젝트3.png";
+
 const Project = ({ elementRef }) => {
   return (
     <div ref={elementRef} className="bg-[#f1f2f6] py-[4rem]">
       <div className="flex flex-col justify-center items-center gap-5">
         <SubTitle title="PROJECT" />
         <div className="bg-[#755567] w-full p-[2rem] py-[4rem] mt-[1rem] flex flex-wrap justify-center gap-5">
-          <ProjectItem />
-          <ProjectItem />
-          <ProjectItem />
+          <ProjectItem 
+            PTitle='차량객체 추출 기반 번호인식' 
+            content='기존 서비스의 빈번한 인식 오류 이슈를 해결하기 위해 차량 객체 인식 및 번호판 추출 AI 모델을 설계하여 높은 정확도로 차량을 인식하고자 한 프로젝트' 
+            image={Project1} 
+            gUrl='https://github.com/maejyomi/license-plate-recognition'/>
+          <ProjectItem 
+            PTitle='폐기물 수수료 검색 및 게시판'
+            content='부산광역시 대형 폐기물 수수료 검색 및 무료 나눔을 할 수 있는 게시판 서비스 프로젝트' 
+            image={Project2} 
+            gUrl='https://github.com/maejyomi/WasteNow-front'/>
+          <ProjectItem 
+            PTitle='AR을 활용한 문화재 소개'
+            content='2021년 동국대학교 졸업 프로젝트로 진행한 것으로, AR을 활용한 문화재 소개 어플리케이션 구현' 
+            image={Project3} 
+            gUrl='https://github.com/maejyomi/Capstone-Design'/>
         </div>
       </div>
     </div>
--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
-const ProjectItem = () => {
-  return <div className="bg-white rounded-lg w-[350px] h-[400px] ">test</div>;
+const ProjectItem = ({PTitle, content, image, gUrl}) => {
+  return (
+    <div className="relative transition-all bg-white rounded-lg w-[350px] h-[400px] p-[2rem] hover:scale-105">
+      <div className="bg-gray-300 w-full h-[150px]">
+        <img src={image} className="w-full h-full object-fill"></img>
+      </div>
+      <div className="mt-[1rem] ">
+        <h3 className="text-xl font-bold">{PTitle}</h3>
+        <p className="mt-[0.5rem]">
+          {content}
+        </p>
+      </div>
+      <div className="absolute bottom-[2rem] cursor-pointer">
+        <a href={gUrl} className="flex items-center gap-2">
+          <FaGithub className="text-2xl" />
+          <p className="text-blue-600 hover:underline">GitHub 보기</p>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectItem;
